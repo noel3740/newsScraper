7894efd77b52b2a9949a0c87b5aa7ab6dfb3846d
Noel Nevarez <noel3740@gmail.com>
2019-02-25 16:21:32 -0600

    Initial server code.

---
+++ server.js
@@ -0,0 +1,28 @@
+//Require dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+//Get the server port
+const PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+//Use url encode and json middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+//Connect to mongo db
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+mongoose.connect(MONGODB_URI);
+
+//require express routes
+const apiRoutes = require("./controllers/apiController");
+app.use("/api", apiRoutes);
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
