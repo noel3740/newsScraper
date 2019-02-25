44863dd048517113f337fa2b80cfae5a997b18d5
Noel Nevarez <noel3740@gmail.com>
2019-02-25 17:01:26 -0600

    Forcing the title of artciles to be unique to prevent duplicates.

--- models/Article.js
+++ models/Article.js
@@ -7,7 +7,8 @@ var Schema = mongoose.Schema;
 var ArticleSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     link: {
         type: String,
