1695dec0b92382cf7a4fd3fec04140f993289bc0
Noel Nevarez <noel3740@gmail.com>
2019-02-28 13:28:59 -0600

    Adding note save, delete, and add functionality.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,25 +2,66 @@ $(document).ready(function () {
     var alertDiv = $("#errorAlert");
     alertDiv.hide();
 
-    //Attach on click event to view notes button
-    $(document).on("click", ".viewNotesButton", function () {
-        //Grab all the notes assocated with this article
-        var articleId = $(this).data("article-id");
+    function displayMessage(message, isError) {
+
+        alertDiv.removeClass("alert-danger alert-success");
+
+        var alertClass = isError ? "alert-danger" : "alert-success";
+        alertDiv.addClass(alertClass);
+
+        alertDiv
+            .text(message)
+            .slideDown();
+
+        setTimeout(() => {
+            alertDiv.slideUp("slow");
+        }, 2000);
+    }
+
+    //Function that creates the div that contains a specific note
+    function createNoteDiv(note, index) {
+        return $(`
+        <div data-note-id=${note._id} class="noteContainer col-12 py-4 ${index !== 0 ? "border-top" : ""}">
+            <div class="row">
+                <div class="editNoteContainer px-2">
+                     <div class="noteEdit">
+                        <textarea data-note-id=${note._id} class="noteTextArea form-control" rows="3" placeholder="Enter note text">${note.text}</textarea>
+                    </div>
+                </div>
+                <div class="noteButtonsContainer">
+                    <div class="mb-2 mr-2">
+                        <button data-note-id=${note._id} ${note.text.trim() !== "" ? "" : "disabled"} class="noteSaveBtn btn btn-secondary btn-block"><i class="fas fa-save"></i> Save</button>
+                    </div>
+                    <div class="mr-2">
+                        <button data-note-id=${note._id} class="noteDeleteBtn btn btn-secondary btn-block ${note._id !== -1 ? "" : "d-none"}"><i class="fas fa-times"></i> Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+    }
+
+    //Function that gets the notes for the current article and populates the notes modal
+    function populateNotes(articleId) {
+        //Enable the add button
+        $("#addNoteButton").prop("disabled", false);
+        
+        var notesModal = $("#notesModal");
+        console.log(articleId);
 
         $.ajax({
             type: "GET",
             url: `/api/articles/${articleId}`,
             success: function (article) {
-                var notesModal = $("#notesModal");
+
                 var notesContainer = notesModal.find(".modal-body");
                 notesContainer.empty();
 
                 console.log(article);
 
-                article.notes.forEach(function (note) {
-                    var noteDiv = $("<div>")
-                        .addClass(`col-12 py-4 mb-1 border rounded`)
-                        .text(note.text);
+                article.notes.forEach(function (note, index) {
+
+                    var noteDiv = createNoteDiv(note, index);
 
                     notesContainer.append(noteDiv);
                 });
@@ -30,69 +71,109 @@ $(document).ready(function () {
             },
             error: function (error) {
                 console.log(error);
-                alertDiv
-                    .text("An error occured getting notes from the database!")
-                    .slideDown();
+                displayMessage("An error occured getting notes from the database!", true);
+            }
+        });
+    }
 
-                setTimeout(() => {
-                    alertDiv.slideUp("slow");
-                }, 2000);
+    //Attach on click event to view notes button
+    $(document).on("click", ".viewNotesButton", function () {
+        //Set the article-id data attribute on the modal
+        var articleId = $(this).data("article-id");
+        $("#notesModal").data("article-id", articleId);
 
+        //Populate the screen with all the notes for the current article
+        populateNotes(articleId);
+    });
+
+    //On input event for note text area
+    $(document).on("input", ".noteTextArea", function (event) {
+        //Enable or disable the save button on the note
+        var noteTextArea = $(event.target);
+        var noteId = noteTextArea.data("note-id");
+        var saveNoteButton = $(`.noteSaveBtn[data-note-id=${noteId}]`);
+        var enableSaveButton = (event.target.value.trim() !== "");
+
+        saveNoteButton.prop("disabled", !enableSaveButton);
+    });
+
+    //On click event for note save button
+    $(document).on("click", ".noteSaveBtn", function () {
+        var articleId = $("#notesModal").data("article-id");
+        var noteId = parseInt($(this).data("note-id"));
+        var httpMethod = noteId !== -1 ? "PUT" : "POST";
+        var apiUrl = noteId !== -1 ? `/api/notes/${noteId}` : `/api/articles/${articleId}/notes`;
+
+        const note = {
+            text: $(`.noteTextArea[data-note-id=${noteId}]`).val()
+        };
+
+        console.log(note);
+        console.log(httpMethod);
+        console.log(apiUrl);
+
+        $.ajax({
+            type: httpMethod,
+            url: apiUrl,
+            data: note,
+            success: function (response) {
+                console.log(response);
+                displayMessage("Successful update/insert of note!");
+                populateNotes(articleId);
+            },
+            error: function (error) {
+                console.log(error);
+                displayMessage("An error occured saving the note to the database!", true);
+            }
+        });
+
+    });
+
+    //On click method for the note delete button
+    $(document).on("click", ".noteDeleteBtn", function () {
+
+        //Send a delete request to the API to delete the note from the database
+        var noteId = $(this).data("note-id");
+
+        $.ajax({
+            type: "DELETE",
+            url: `/api/notes/${noteId}`,
+            success: function (response) {
+                console.log(response);
+                displayMessage("Successful deletion of note!");
+
+                //Slowly remove the note with animation
+                $(`.noteContainer[data-note-id=${noteId}]`)
+                    .hide("slow", function () { $(this).remove(); });
+
+            },
+            error: function (error) {
+                console.log(error);
+                displayMessage("An error occured deleting the note from the database!", true);
             }
         });
     });
 
-    // function createArticleCards() {
-    //     $(".articlesDiv").empty();
+    $("#addNoteButton").on("click", function () {
+        //Disable the add button until they hit save on that new note
+        $(this).prop("disabled", true);
 
-    //     //Get articles from the api
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "/api/articles",
-    //         success: function (articles) {
-    //             $(".articlesDiv").empty();
+        var notesModal = $("#notesModal");
+        var notesContainer = notesModal.find(".modal-body");
 
-    //             //Loop thru all the articles obtained and create a card on the articlesDiv
-    //             articles.forEach(function (article, index) {
-    //                 var newCard = $(`
-    //                 <div class="col-12">
-    //                     <div class="card articleCard">
-    //                         <div class="row">
-    //                             <div class="col-lg-3 col-md-12 mt-4">
-    //                                 <img src="${article.imageUrl}"
-    //                                     alt="...">
-    //                             </div>
-    //                             <div class="col-lg-9 col-md-12">
-    //                                 <div class="card-body">
-    //                                     <h3><a href="${article.link}"
-    //                                             target="_blank">${article.title}</a>
-    //                                     </h3>
-    //                                     <p class="card-text">${article.text}</p>
-    //                                     <div class="articleButtonDiv">
-    //                                         <button data-article-id="${article._id}" class="btn btn-primary float-right viewNotesButton">View Notes</button>
-    //                                     </div>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //                 ${(index + 1) < articles.length ? "<hr class=\"w-100\">" : ""}`);
+        var newNote = {
+            text: "",
+            _id: -1
+        };
 
-    //                 $(".articlesDiv").append(newCard);
-    //             });
-    //         }
-    //     });
-    // }
+        var index = $(".noteContainer").length;
+        var noteDiv = createNoteDiv(newNote, index);
+        notesContainer.append(noteDiv);
 
-    // //Populate the database with the latest articles then create the article cards
-    // $.ajax({
-    //     type: "GET",
-    //     url: "/api/scrape",
-    //     success: function (response) {
-    //         console.log(response);
-    //         createArticleCards();
-    //     }
-    // });
+        noteDiv.find(".noteTextArea").focus();
 
+        //Scroll to the bottom of the notes container
+        notesContainer.animate({ scrollTop: $(document).height() }, "slow");
+    });
 
 });
